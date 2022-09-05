b605c207ba904b6cb11e812a21fa595411af924c
Mark Lewis <mlewis@trinity.edu>
2022-09-05 18:53:39 -0500

    Compiler flags and C++ version.

--- RustVersion/src/kd_tree.rs
+++ RustVersion/src/kd_tree.rs
@@ -84,7 +84,7 @@ pub fn build_tree<'a>(
         }
         cm /= f64x4::splat(m);
         let mut split_dim = 0;
-        for dim in 1..2 {
+        for dim in 1..3 {
             if max[dim] - min[dim] > max[split_dim] - min[split_dim] {
                 split_dim = dim
             }
@@ -284,7 +284,7 @@ mod tests {
     fn two_leaves() {
         let parts = simd_particle::circular_orbits(11);
         let mut node_vec = kd_tree::allocate_node_vec(parts.len());
-        assert_eq!(node_vec.len(), 4);
+        assert_eq!(node_vec.len(), 6);
         let mut indices: Vec<usize> = (0..parts.len()).collect();
         kd_tree::build_tree(&mut indices, 0, parts.len(), &parts, 0, &mut node_vec);
         kd_tree::recur_test_tree_struct(
@@ -295,9 +295,7 @@ mod tests {
             f64x4::splat(1e100),
         );
         assert_eq!(node_vec[0].num_parts, 0);
-        assert_eq!(node_vec[0].split_dim, 0);
-        assert_eq!(node_vec[1].num_parts, 5);
-        assert_eq!(node_vec[2].num_parts, 6);
+        assert_eq!(node_vec[1].num_parts + node_vec[2].num_parts, 12);
     }
 
     #[test]
@@ -318,7 +316,7 @@ mod tests {
     #[test]
     fn big_solar_with_steps() {
         let mut parts = simd_particle::circular_orbits(5000);
-        kd_tree::simple_sim(&mut parts, 1e-3, 100);
+        kd_tree::simple_sim(&mut parts, 1e-3, 10);
 
         let mut node_vec = kd_tree::allocate_node_vec(parts.len());
         let mut indices: Vec<usize> = (0..parts.len()).collect();
